050d1e2015df00f82dc6f2e883276e78161c44c4
Rowan12628 <170511926+Rowan12628@users.noreply.github.com>
2024-07-12 14:39:25 +0100

    feature: AddAuthor functionality complete

--- src/components/addAuthor/AddAuthor.jsx
+++ src/components/addAuthor/AddAuthor.jsx
@@ -1,18 +1,26 @@
 import { useState } from "react";
 
 import { changeHandler } from "../../utils/helpers";
+import { addAuthor } from "../../utils/authorFetch";
 
 const AddAuthor = () => {
   const [author, setAuthor] = useState("");
-  console.log(author);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    console.log("submit handler AddAuthor");
+
+    const data = await addAuthor(author);
+    console.log("data in addAuthor: ", data);
+  };
   return (
     <div>
-      <form>
+      <form onSubmit={submitHandler}>
         <input
           placeholder="Author name..."
           onChange={(e) => changeHandler(e, setAuthor)}
         />
-        <button>Add author</button>
+        <button type="submit">Add author</button>
       </form>
     </div>
   );
