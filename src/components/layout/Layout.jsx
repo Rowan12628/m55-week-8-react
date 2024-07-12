781bc5912adeebb4b4e874e18712c2f87c685b33
Rowan12628 <170511926+Rowan12628@users.noreply.github.com>
2024-07-12 13:49:14 +0100

    feature: MainContainer, MessageContainer

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -3,15 +3,19 @@ import React from "react";
 import Header from "../header/Header";
 import Footer from "../footer/Footer";
 
+import { MessageProvider } from "../../providers/messageProvider/MessageProvider";
+
 import "./Layout.css";
 
 const Layout = ({ children }) => {
   return (
     <>
       <div className="layout">
-        <Header />
-        {children}
-        <Footer />
+        <MessageProvider>
+          <Header />
+          {children}
+          <Footer />
+        </MessageProvider>
       </div>
     </>
   );
